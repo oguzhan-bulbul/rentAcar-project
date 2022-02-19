090459efcf155970dd545fa13588afe807243494
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-02-19 10:30:18 +0300

    nothwind project 19.02.2022

--- northwind/src/main/java/com/turkcell/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/com/turkcell/northwind/business/abstracts/ProductService.java
@@ -6,8 +6,8 @@ import com.turkcell.northwind.business.dtos.ProductListDto;
 import com.turkcell.northwind.business.requests.CreateProductRequest;
 
 public interface ProductService {
-	List<ProductListDto> getAll();
 	
+	List<ProductListDto> getAll();	
 	void add(CreateProductRequest createProductRequest);
 	
 
